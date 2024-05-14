d73e54579ba5faf07e659ad24b80f69268402bbd
aly <alyssonrosa1@gmail.com>
2024-05-14 19:23:40 -0300

    copilot

--- lua/custom/plugins/copilot.lua
+++ lua/custom/plugins/copilot.lua
@@ -1,3 +1,7 @@
+-- Disables the default tab mapping and sets a new one to <C-J>
+vim.g.copilot_no_tab_map = true
+vim.api.nvim_set_keymap('i', '<C-J>', 'copilot#Accept("<CR>")', { silent = true, expr = true })
+
 return {
   { 'github/copilot.vim' },
 }
