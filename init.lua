6b469c1d91ccff4308fdbf6fac0158d6973d25b7
aly <alyssonrosa1@gmail.com>
2024-07-23 09:11:38 -0300

    Add harpoon

--- init.lua
+++ init.lua
@@ -168,6 +168,8 @@ vim.keymap.set('v', 'p', '"_dP', { silent = true })
 vim.opt.hlsearch = true
 vim.keymap.set('n', '<Esc>', '<cmd>nohlsearch<CR>')
 
+vim.keymap.set('n', '<leader>l', '<cmd>Explore<CR>', { desc = 'Go to :Explore' })
+
 -- Diagnostic keymaps
 vim.keymap.set('n', '[d', vim.diagnostic.goto_prev, { desc = 'Go to previous [D]iagnostic message' })
 vim.keymap.set('n', ']d', vim.diagnostic.goto_next, { desc = 'Go to next [D]iagnostic message' })
