8ae02bf59edd81d04ed4c13a20203b48944c0c61
aly <alyssonrosa1@gmail.com>
2024-08-19 13:56:10 -0300

    path_display truncate

--- init.lua
+++ init.lua
@@ -379,7 +379,7 @@ require('lazy').setup({
         --  All the info you're looking for is in `:help telescope.setup()`
         --
         defaults = {
-          path_display = { 'smart' },
+          path_display = { 'truncate' },
           --   mappings = {
           --     i = { ['<c-enter>'] = 'to_fuzzy_refine' },
           --   },
