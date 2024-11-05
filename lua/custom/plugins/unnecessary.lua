75f4edb4a9f05b23a4fab82c379f1f4e15b98ddc
aly <alyssonrosa1@gmail.com>
2024-11-05 13:30:19 -0300

    Some changes

--- lua/custom/plugins/unnecessary.lua
+++ lua/custom/plugins/unnecessary.lua
@@ -3,5 +3,6 @@ return {
   { 'tjdevries/sPoNGe-BoB.NvIm' },
   { 'alec-gibson/nvim-tetris' },
   { 'tamton-aquib/zone.nvim' },
+  { 'mistricky/codesnap.nvim', build = 'make' },
   { 'Eandrju/cellular-automaton.nvim' },
 }
